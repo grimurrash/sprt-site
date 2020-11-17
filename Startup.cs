7f20c8f30a8c0a367177ab1dedd1308c01bafc24
rashi <rashit.sabirov1999@gmail.com>
2020-11-17 22:46:48 +0300

    Settings for running on the server

--- Startup.cs
+++ Startup.cs
@@ -92,8 +92,18 @@ namespace NewSprt
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-            app.UseStatusCodePages();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();            
+                app.UseStatusCodePages();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
